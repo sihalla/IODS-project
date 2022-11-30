bea160be3484c0d79fc1b31b5fdc0a1433e9897c
unknown <sini.hautala@helsinki.fi>
2022-11-30 14:03:22 +0200

    Week 5

--- create_human.R
+++ create_human.R
@@ -1,6 +1,6 @@
 #Author:Sini Hautala
 #Date: 24.11.2022
-#Data wrangling week 4
+#Data wrangling week 4-5
 
 
 #install.packages("boot")
@@ -20,7 +20,7 @@ View(hd)
 View(gii)
 
 #Give more informative names to columns
-colnames(hd) <- c('HDRank','Country', 'HDI','LifeExp','EduYExp', 'EduYMean', 'Income', 'GNI-HDI')
+colnames(hd) <- c('HDRank','Country', 'HDI','LifeExp','EduYExp', 'EduYMean', 'Income', 'GNI')
 colnames(gii) <- c('GIIRank','Country','GII', 'Mdeaths', 'Alabours', 'parlament', 'edu2F', 'edu2M', 'workF', 'workM')
 
 #Mutate gender inequality data.  
@@ -49,4 +49,41 @@ View(hd_gii)
 # hd_gii has 195 observations and 19 variables as it should.
 
 #saving
-write.csv(hd_gii, "./data/human.csv")
+#write.csv(hd_gii, "./data/human.csv")
+
+
+
+# access the stringr and dplyr package 
+library(stringr)
+library(dplyr)
+
+#transform GNI variable to numeric (using string manipulation)
+hd_gii$GNI
+str(hd_gii$GNI)
+
+# remove the commas from GNI and print out a numeric version of it
+hd_gii$GNI <- gsub(",", ".", hd_gii$GNI) %>% as.numeric
+
+#Exclude unneeded variables
+keep_columns <- c("Country", "edu2M", "workF", "LifeExp", "EduYExp", "GNI", "Mdeaths", "Alabours", "parlament")
+hd_gii_ <- dplyr::select(hd_gii, one_of(keep_columns))
+
+# remove rows with missing values
+hd_gii_F <- filter(hd_gii_, complete.cases(hd_gii_))
+
+#Remove the observations which relate to regions instead of countries. Regions were already automatically removed when NA values were removed. Only were 155 rows left. Here regions removed from the original data.
+#hd_gii_F2 <- hd_gii %>% top_n(188, HDRank)
+
+#Define the row names of the data by the country names and remove the country name 
+rownames(hd_gii_F) <- hd_gii_F$Country
+hd_gii_F
+View(hd_gii_F)
+
+#remove the country name column from the data
+hd_gii_F<- dplyr::select(hd_gii_F, -Country)
+str(hd_gii_F)
+
+write_csv(hd_gii_F, "human.csv")
+#read_csv("human.csv")
+
+
